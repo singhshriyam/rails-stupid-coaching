1febff79c9f1026e9f614c82970dabfdaa25bd07
Shriyam Singh <singh.shriyam@gmail.com>
2024-10-14 09:04:38 +0200

    coaching

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,13 @@
+# define answer controller
+class AnswersController < ApplicationController
+  def answer
+    @question = params[:question]
+    if @question == 'I am going to work'
+      @answer = 'Great!'
+    elsif @question&.end_with?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,6 @@
+# creating a controller for questions
+class QuestionsController < ApplicationController
+  def ask
+    # This action doesn't need to set any instance variables for now
+  end
+end
